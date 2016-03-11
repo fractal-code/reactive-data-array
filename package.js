d026251a4f977b2423d2a2492e01588ba3066ee2
Rami Jarrar <rami@talos.email>
2016-03-12 08:43:50 +1100

    Add boilerplate code

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'talos:reactive-data-array',
+  version: '0.1.0',
+  summary: 'Create reactive arrays that track cursor data',
+  git: 'https://github.com/talos-code/reactive-data-array'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.addFiles('reactive-data-array.js');
+});
