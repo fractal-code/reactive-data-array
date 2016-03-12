b67d4e6edc43b77884d8a29a93a7076bad4ee00e
Rami Jarrar <rami@talos.email>
2016-03-12 22:01:22 +1100

    Bump version to 1.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'talos:reactive-data-array',
-  version: '0.2.0',
+  version: '1.0.0',
   summary: 'Create reactive arrays that track cursor data',
   git: 'https://github.com/talos-code/reactive-data-array'
 });
