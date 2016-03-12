9416c2cf1ea566b008eefff830363b9156750ffb
Rami Jarrar <rami@talos.email>
2016-03-12 18:01:15 +1100

    Register reactive dependency on arrays

--- reactive-data-array.js
+++ reactive-data-array.js
@@ -1,28 +1,38 @@
 ReactiveDataArray = function (targetArray, cursor, callbacks = {}) {
+    var arrayDep = new Tracker.Dependency;
+
+    // Get index of doc inside target array using the _id
     var atIndex = function (id) {
         return targetArray.findIndex(function (doc) {
             return doc._id === id;
         });
     };
 
-    // Provide handle to stop live query
-    return cursor.observeChanges({
+    var liveQuery = cursor.observeChanges({
         added: function (id, fields) {
             var doc = Object.assign({_id: id}, fields);
             targetArray.push(doc);
 
-            if (callbacks.added) {callbacks.added(doc);}
+            if (callbacks.added) {callbacks.added(doc)}
+            arrayDep.changed();
         },
         changed: function (id, fields) {
-            // Merge (overwrite) new fields into target doc
+            // Merge (overwrite) changed fields into target doc
             var newDoc = Object.assign(targetArray[atIndex(id)], fields);
 
             if (callbacks.changed) {callbacks.changed(newDoc);}
+            arrayDep.changed();
         },
         removed: function (id) {
             var oldDoc = targetArray.splice(atIndex(id), 1);
 
             if (callbacks.removed) {callbacks.removed(oldDoc);}
+            arrayDep.changed();
         }
     });
+
+    return {
+        stop: liveQuery.stop,
+        dep: arrayDep
+    }
 };
