32f95a85055e6b61abd4e8e8358a78c74941287e
Rami Jarrar <rami@talos.email>
2016-03-12 21:58:53 +1100

    Suppress initial 'added' callbacks

--- reactive-data-array.js
+++ reactive-data-array.js
@@ -1,6 +1,4 @@
 ReactiveDataArray = function (targetArray, cursor, callbacks = {}) {
-    var arrayDep = new Tracker.Dependency;
-
     // Get index of doc inside target array using the _id
     var atIndex = function (id) {
         return targetArray.findIndex(function (doc) {
@@ -8,31 +6,26 @@ ReactiveDataArray = function (targetArray, cursor, callbacks = {}) {
         });
     };
 
-    var liveQuery = cursor.observeChanges({
+    return cursor.observeChanges({
         added: function (id, fields) {
             var doc = Object.assign({_id: id}, fields);
             targetArray.push(doc);
 
-            if (callbacks.added) {callbacks.added(doc)}
-            arrayDep.changed();
+            // Suppress callbacks on initial calls for existing docs
+            if (targetArray.length === cursor.count()) {
+                if (callbacks.added) {callbacks.added(doc)}
+            }
         },
         changed: function (id, fields) {
             // Merge (overwrite) changed fields into target doc
             var newDoc = Object.assign(targetArray[atIndex(id)], fields);
 
             if (callbacks.changed) {callbacks.changed(newDoc);}
-            arrayDep.changed();
         },
         removed: function (id) {
             var oldDoc = targetArray.splice(atIndex(id), 1);
 
             if (callbacks.removed) {callbacks.removed(oldDoc);}
-            arrayDep.changed();
         }
     });
-
-    return {
-        stop: liveQuery.stop,
-        dep: arrayDep
-    }
 };
