1e156d43c167e04691e3a0a01d8df43700339e5d
Rami Jarrar <rami@talos.email>
2016-04-02 21:20:30 +1100

    Notify user of startup call

--- reactive-data-array.js
+++ reactive-data-array.js
@@ -1,4 +1,6 @@
 ReactiveDataArray = function (targetArray, cursor, callback) {
+    var startupCall = true;
+
     // Get index of doc inside target array using the _id
     var atIndex = function (id) {
         return targetArray.findIndex(function (doc) {
@@ -11,21 +13,31 @@ ReactiveDataArray = function (targetArray, cursor, callback) {
             var doc = Object.assign({_id: id}, fields);
             targetArray.push(doc);
 
-            // Suppress callbacks on initial calls for existing docs
+            // Suppress callbacks on calls for existing docs
             if (targetArray.length === cursor.count()) {
-                if (callback) {callback("added", doc)}
+                if (!callback) {return;}
+
+                callback({
+                    type: "added",
+                    startup: startupCall,
+                    doc: doc
+                });
+
+                startupCall = false;
             }
         },
         changed: function (id, fields) {
             // Merge (overwrite) changed fields into target doc
             var newDoc = Object.assign(targetArray[atIndex(id)], fields);
 
-            if (callback) {callback("changed", newDoc);}
+            if (!callback) {return;}
+            callback({type: "changed", doc: newDoc});
         },
         removed: function (id) {
             var oldDoc = targetArray.splice(atIndex(id), 1);
 
-            if (callback) {callback("removed", oldDoc);}
+            if (!callback) {return;}
+            callback({type: "removed", doc: oldDoc});
         }
     });
 };
